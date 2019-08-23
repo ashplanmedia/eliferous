ab4af0ecb23125565f0e5e9ccbb340044301103d
root <root@ip-172-31-1-213.ap-south-1.compute.internal>
2019-08-23 14:01:55 +0000

    Website Changes 23 Aug

---
+++ pub/static/adminhtml/Magento/backend/en_US/Magento_AdvancedSearch/js/testconnection.js
@@ -0,0 +1,1 @@
+/var/www/eliferous/vendor/magento/module-advanced-search/view/adminhtml/web/js/testconnection.js
---
+++ pub/static/adminhtml/Magento/backend/en_US/Magento_User/js/delete-user-account.js
@@ -0,0 +1,1 @@
+/var/www/eliferous/vendor/magento/module-user/view/adminhtml/web/js/delete-user-account.js
